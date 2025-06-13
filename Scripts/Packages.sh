7649150d59bce39a6815cfe72cfd4d040ba03f36
Starktomy <37720599+Starktomy@users.noreply.github.com>
2025-06-14 07:52:53 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -67,7 +67,9 @@ UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base q
 UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "istore" "linkease/nas-packages-luci" "main"
+https://github.com/linkease/nas-packages
+UPDATE_PACKAGE "nas-packages" "linkease/nas-packages" "main"
+UPDATE_PACKAGE "nas-packages-luci" "linkease/nas-packages-luci" "main"
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
